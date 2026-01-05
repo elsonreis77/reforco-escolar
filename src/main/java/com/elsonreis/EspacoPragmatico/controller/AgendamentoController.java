9690bf7b9f0d086a94c0d910efe2a9f4050bee8f
elsonreis77 <elsonreis084@gmail.com>
2026-01-05 19:00:36 -0300

    Classe AgendamentoController

--- src/main/java/com/elsonreis/EspacoPragmatico/controller/AgendamentoController.java
+++ src/main/java/com/elsonreis/EspacoPragmatico/controller/AgendamentoController.java
@@ -1,4 +1,28 @@
 package com.elsonreis.EspacoPragmatico.controller;
 
+
+import com.elsonreis.EspacoPragmatico.model.Agendamento;
+import com.elsonreis.EspacoPragmatico.service.AgendamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamentos")
+@CrossOrigin
 public class AgendamentoController {
+
+    @Autowired
+    private AgendamentoService service;
+
+    @PostMapping
+    public Agendamento criar(@RequestBody Agendamento agendamento) {
+        return service.salvar(agendamento);
+    }
+
+    @GetMapping
+    public List<Agendamento> listar() {
+        return service.listar();
+    }
 }
