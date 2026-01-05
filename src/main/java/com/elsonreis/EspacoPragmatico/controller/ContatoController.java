4ef1d0cc4bf8c7723410d1678a25246f6736ab1d
elsonreis77 <elsonreis084@gmail.com>
2026-01-05 19:18:19 -0300

    classes controller

--- src/main/java/com/elsonreis/EspacoPragmatico/controller/ContatoController.java
+++ src/main/java/com/elsonreis/EspacoPragmatico/controller/ContatoController.java
@@ -1,4 +1,22 @@
 package com.elsonreis.EspacoPragmatico.controller;
 
+import com.elsonreis.EspacoPragmatico.model.Contato;
+import com.elsonreis.EspacoPragmatico.service.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contatos")
+@CrossOrigin
 public class ContatoController {
+
+    @Autowired
+    private ContatoService service;
+
+    @RequestMapping
+    public Contato salvar(@RequestBody Contato contato) {
+        return service.salvar(contato);
+    }
 }
