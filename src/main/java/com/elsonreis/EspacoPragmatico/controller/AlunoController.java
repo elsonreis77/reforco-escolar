006db9120e91ba75ece22fb28f7b15797ea63eb1
elsonreis77 <elsonreis084@gmail.com>
2026-01-05 18:55:31 -0300

    Classe AlunoController

--- src/main/java/com/elsonreis/EspacoPragmatico/controller/AlunoController.java
+++ src/main/java/com/elsonreis/EspacoPragmatico/controller/AlunoController.java
@@ -3,12 +3,15 @@ package com.elsonreis.EspacoPragmatico.controller;
 import com.elsonreis.EspacoPragmatico.model.Aluno;
 import com.elsonreis.EspacoPragmatico.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
+@RestController
+@RequestMapping("/alunos")
+@CrossOrigin
 public class AlunoController {
 
     @Autowired
